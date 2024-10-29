26a4758124e348cccd24c3d6924d8684bd7e40ed
minhthang <minhthangqhqn@gmail.com>
2024-10-29 21:20:06 +0700

    Add project files.

---
+++ DataAccessLayer/BookingDAO.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAccessLayer
+{
+    public class BookingDAO
+    {
+        public static List<BookingReservation> GetBookings()
+        {
+
+        }
+    }
+}
---
+++ DataAccessLayer/CustomerDAO.cs
@@ -0,0 +1,69 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CustomerDAO
+    {
+        public static List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            try
+            {
+                using var context = new FuminiHotelManagementContext();
+                customers = context.Customers.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return customers;
+        }
+
+        public static void CreateNewCustomer(Customer customer)
+        {
+            try
+            {
+                using var context = new FuminiHotelManagementContext();
+                context.Customers.Add(customer);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static void UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                using var context = new FuminiHotelManagementContext();
+                context.Customers.Update(customer);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static void DeleteCustomer(Customer customer)
+        {
+            try
+            {
+                using var context = new FuminiHotelManagementContext();
+                context.Customers.Remove(customer);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
